0327279a7345f97dbbc617e9aae1e4caaf6d5780
Roy Russo <royrusso@gmail.com>
2013-07-19 17:47:10 -0400

    fixes #58 and fixes #59

--- js/template/QueryTemplate.js
+++ js/template/QueryTemplate.js
@@ -49,22 +49,30 @@ queryTemplate.view = [
 ].join("\n");
 
 queryTemplate.results = [
+
+    '<div class="alert alert-info">',
+    '<h4>Warning!</h4>',
+    'The Query UI is still experimental and is not in a finished state!',
+    '</div>',
+
     '<div class="pull-left muted"><small><%- results.totalHits %> Hits returned in <%- results.responseTime %>ms</small></div>',
     '<div class="center-table">',
     '<table class="table table-bordered table-striped table-hover" id="resultsTable">',
     '<thead><tr>',
+    //'<th></th>',
     '<% _.each(columns, function(col) { %>',
     '<% if (col.type == "source" )  { %> <th style="color: green"> ',
     '<% } else { %> <th> <% } %>',
-    '<%- col.name %></th>',
+    '<span style="font-size: 13px;"><%- col.name %></span></th>',
     '<% }); %>',
     '</tr>',
     '</thead>',
     '<tbody>',
     '<% _.each(results.results, function(hit, key) { %>',
     '<tr>',
+    //'<td><button class="btn"><i class="icon-code" style="font-size: 12px;"></i></button> </td>',
     '<% _.each(columns, function(col) { %>',
-    '<td><%- hit[col.key] %></td>',
+    '<td style="font-size: 12px;"><%- hit[col.key] %></td>',
     '<% }); %>',
     '</tr>',
     '<% }); %>',
