c604a5e6b09f38bcf5083add298e6611f66c996b
Roy Russo <royrusso@gmail.com>
2013-09-20 11:22:36 -0400

    updated version to .99

--- js/util/constants.js
+++ js/util/constants.js
@@ -16,7 +16,7 @@
  Latest Builds: https://github.com/royrusso/elasticsearch-HQ
  */
 
-var HQVERSION = .98;
+var HQVERSION = .99;
 
 var REMOTE_API_PATH = 'http://www.elastichq.org/api';
 //var REMOTE_API_PATH = 'http://local.dev/elastichqorg/api';
