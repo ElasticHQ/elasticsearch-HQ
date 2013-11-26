da6c268caf4881ded3c45205b2cfc7f1254c808e
Roy Russo <royrusso@gmail.com>
2013-11-26 15:55:50 -0500

    fixes #109

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,8 @@ module.exports = function (grunt) {
 
     var config = {
         pkg:grunt.file.readJSON('package.json'),
+        jsDir:'js',
+        cssDir:'css',
         distDir:'dist',
         tempDir:'tmp',
         meta:{
@@ -14,12 +16,15 @@ module.exports = function (grunt) {
                 '* License: <%= pkg.license %> \n*/\n\n'
         },
         clean:{
-            dist:['dist']
+            dist:['dist'],
+            cssAll:["<%= cssDir %>/all.min.css"],
+            jsAll:["<%= jsDir %>/all.min.js"],
+            temp:["<%= tempDir %>"]
         },
         cssmin:{
             combine:{
                 files:{
-                    'dist/css/output.css':['dist/css/elastichq.min.css', 'dist/css/pnotify.min.css']
+                    'dist/css/output.css':['dist/css/*.min.css', 'dist/css/pnotify.min.css']
                 },
                 options:{
                     banner:'<%= meta.banner %>'
@@ -27,12 +32,14 @@ module.exports = function (grunt) {
             },
             minify:{
                 expand:true,
-                cwd:'css/',
-                src:['*.css', '!*.min.css'],
-                dest:'<%= distDir %>/css/',
+                cwd:"<%= tempDir %>",
+                src:['*.css'], // , '!*.min.css'],
+                dest:'<%= cssDir %>',
                 ext:'.min.css',
                 options:{
-                    report:'min'
+                    keepSpecialComments:0,
+                    report:'min',
+                    banner:'<%= meta.banner %>'
                 }
             }
         },
@@ -40,15 +47,19 @@ module.exports = function (grunt) {
             copycss:{
                 cwd:'css/',
                 expand:true,
-                src:['**/*.min.css'],
-                dest:'<%= distDir %>/css'
+                src:['***/*//*.min.css'],
+                dest:'<%= tmp %>/css'
             }
         },
         uglify:{
             files:{
-                src:['js/*.js', '!lib/*'], // source files mask
-                //src: ['**/*.js', '!config.js', '!app/dashboards/*.js'],
-                dest:'dist', // destination folder
+                src:[
+                    //'js*//*.js',
+                    //'js/model/util/ModelUtil.js' ,
+                    "<%= tempDir %>/all.js"//,
+                    //'!lib*//*'
+                ],
+                dest:"<%= jsDir %>", // destination folder
                 expand:true, // allow dynamic building
                 flatten:true, // remove all unnecessary nesting
                 ext:'.min.js'   // .js to .min.js
@@ -56,13 +67,55 @@ module.exports = function (grunt) {
             options:{
                 quite:true,
                 compress:true,
+                report:'min',
+                //mangle:false,
                 preserveComments:false,
                 banner:'<%= meta.banner %>'
             }
         },
-        watch:{
-            js:{ files:'js*//*.js', tasks:[ 'uglify' ] }
-        }};
+        concat:{
+            css:{
+                src:[
+                    'css//*.css'
+                ],
+                dest:'<%= tempDir %>/all.css'
+            },
+            js:{
+                options:{
+                    stripBanners:true,
+                },
+                src:[
+                    'js/template*//*.js',
+                    'js/model//*//*.js',
+                    'js/model//index//action//*.js',
+                    'js/collection/*/*.js',
+                    'js/view*//*//*.js',
+                    'js/view*//index//action//*.js',
+                    'js/view*//mapping//action//*.js',
+                    'js/route*//*.js',
+                    'js/util*//*.js' ,
+                    '!js/util/constants.js',
+                    '!js/model/util/ModelUtil.js',
+                    '!js/lib*//*'
+                ],
+                dest:'<%= tempDir %>/all.js'
+            }
+        },
+        targethtml:{
+            dist:{
+                files:{
+                    'index.html':'tpl/index.html'
+                }
+            },
+            dev:{
+                files:{
+                    'index.html':'tpl/index.html'
+                }
+            }
+        }/*,
+         watch:{
+         js:{ files:'js*//**//*.js', tasks:[ 'uglify' ] }
+         }*/};
 
     // load plugins
     grunt.loadNpmTasks('grunt-contrib-watch');
@@ -71,18 +124,23 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-string-replace');
+    grunt.loadNpmTasks('grunt-targethtml');
 
     grunt.initConfig(config);
 
-    grunt.registerTask('default', [ 'clean', 'uglify', 'copy:copycss', 'cssmin:minify', 'cssmin:combine' ]);
+    // grunt.registerTask('default', [ 'clean', 'concat', 'cssmin:minify', 'uglify']);
+    grunt.registerTask('default', [ 'clean']);
+
+    grunt.registerTask('dist', [ 'clean', 'concat:css', 'cssmin:minify', 'concat:js', 'uglify', 'targethtml:dist']);
+
+    grunt.registerTask('dev', [ 'clean:temp', 'targethtml:dev']);
 
     // Concat and Minify the src directory into dist
-    grunt.registerTask('build-dev',
-        [
-            'clean',
-            'uglify',
-            'cssmin:minify',
-            'cssmin:combine' ]
-    );
+    /*    grunt.registerTask('build-dev',
+     [
+     'clean',
+     'uglify',
+     'cssmin:minify',
+     'cssmin:combine' ]
+     );*/
 };
