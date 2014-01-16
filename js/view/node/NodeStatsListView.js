dc6a2f59cf983540613186dc7b81289d5168f4fa
Roy Russo <royrusso@gmail.com>
2014-01-16 10:48:03 -0500

    fixes #124

--- js/view/node/NodeStatsListView.js
+++ js/view/node/NodeStatsListView.js
@@ -98,7 +98,8 @@ var NodeStatsListView = Backbone.View.extend(
                         node.stats.jvm.gc.collectors = {};
                         node.stats.jvm.gc.collectors.ConcurrentMarkSweep = {};
                         node.stats.jvm.gc.collectors.ParNew = {};
-
+                        node.stats.jvm.gc.collectors['G1 Young Generation'] = {};
+                        node.stats.jvm.gc.collectors['G1 Old Generation'] = {};
                     }
                     if (!node.stats.os) {
                         osVal = false;
@@ -163,12 +164,24 @@ var NodeStatsListView = Backbone.View.extend(
                     node.stats.heapused = node.stats.jvm.mem.heap_used_in_bytes / node.stats.jvm.mem.heap_committed_in_bytes;
 
                     node.stats.gcfreq = 0;
+                    node.stats.g1gcfreq = 0;
                     node.stats.gcduration = 0;
+                    node.stats.g1gcduration = 0;
                     try {
                         if (node.stats.jvm.gc.collectors.ConcurrentMarkSweep.collection_count === 0) {
                             node.stats.gcfreq = 0;
                         } else {
                             node.stats.gcfreq = node.stats.jvm.uptime_in_millis / node.stats.jvm.gc.collectors.ConcurrentMarkSweep.collection_count / 1000;
+                        }
+                    }
+                    catch (e) {
+                        // default to 0;
+                    }
+                    try {
+                        if (node.stats.jvm.gc.collectors['G1 Young Generation'].collection_count === 0) {
+                            node.stats.g1gcfreq = 0;
+                        } else {
+                            node.stats.g1gcfreq = node.stats.jvm.uptime_in_millis / node.stats.jvm.gc.collectors['G1 Young Generation'].collection_count / 1000;
                         }
                     }
                     catch (e) {
@@ -181,6 +194,12 @@ var NodeStatsListView = Backbone.View.extend(
                     catch (e) {
                         // default to 0
                     }
+                    try {
+                        node.stats.g1gcduration = node.stats.jvm.gc.collectors['G1 Young Generation'].collection_time_in_millis / node.stats.jvm.gc.collectors['G1 Young Generation'].collection_count;
+                    }
+                    catch (e) {
+                        // default to 0
+                    }
 
                     if (node.stats.jvm.gc.collectors.ParNew) {
                         node.stats.gcparnew = node.stats.jvm.uptime_in_millis / node.stats.jvm.gc.collectors.ParNew.collection_count / 1000;
@@ -189,6 +208,14 @@ var NodeStatsListView = Backbone.View.extend(
                     else {
                         node.stats.gcparnew = 0;
                         node.stats.gcparnewduration = 0;
+                    }
+                    if (node.stats.jvm.gc.collectors['G1 Old Generation'] && node.stats.jvm.gc.collectors['G1 Old Generation'].collection_count !== 0) {
+                        node.stats.g1gcold = node.stats.jvm.uptime_in_millis / node.stats.jvm.gc.collectors['G1 Old Generation'].collection_count / 1000;
+                        node.stats.g1gcoldduration = node.stats.jvm.gc.collectors['G1 Old Generation'].collection_time_in_millis / node.stats.jvm.gc.collectors['G1 Old Generation'].collection_count;
+                    }
+                    else {
+                        node.stats.g1gcold = 0;
+                        node.stats.g1gcoldduration = 0;
                     }
 
                     node.stats.swap = 0;
