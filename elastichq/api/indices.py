496a4adce536f0aecaf6415f8092deeb0e51ec10
Roy Russo <royrusso@gmail.com>
2018-01-06 00:07:27 -0500

    - added index apis

--- elastichq/api/indices.py
+++ elastichq/api/indices.py
@@ -1,6 +1,7 @@
 __author__ = 'royrusso'
 
 from flask_restful import Resource
+from flask import request
 
 from ..common.status_codes import HTTP_Status
 from . import api
@@ -22,5 +23,82 @@ class IndexList(Resource):
         response = IndicesService().get_indices(cluster_name, index_name)
         return APIResponse(response, HTTP_Status.OK, None)
 
+
+class Index(Resource):
+    @request_wrapper
+    def get(self, cluster_name, index_name):
+        response = IndicesService().get_indices(cluster_name, index_name)
+        return APIResponse(response, HTTP_Status.OK, None)
+
+    @request_wrapper
+    def delete(self, cluster_name, index_name):
+        response = IndicesService().delete_indices(cluster_name, index_name)
+        return APIResponse(response, HTTP_Status.OK, None)
+
+    @request_wrapper
+    def post(self, cluster_name, index_name):
+        json_data = request.get_json(force=True)
+        params = request.values.to_dict()
+        params.update(json_data)
+
+        response = IndicesService().create_index(cluster_name, index_name, settings=params.get('settings'))
+        return APIResponse(response, HTTP_Status.OK, None)
+
+
+class IndexAction(Resource):
+    @request_wrapper
+    def put(self, cluster_name, index_name, action):
+        response = {}
+
+        if action == '_open':
+            response = IndicesService().open_index(cluster_name, index_name)
+        elif action == '_close':
+            response = IndicesService().close_index(cluster_name, index_name)
+        elif action == '_flush':
+            response = IndicesService().flush_index(cluster_name, index_name)
+        elif action == '_refresh':
+            response = IndicesService().refresh_index(cluster_name, index_name)
+        elif action == '_cache':
+            response = IndicesService().clear_cache(cluster_name, index_name)
+        elif action == '_forcemerge':
+            response = IndicesService().force_merge(cluster_name, index_name)
+
+        return APIResponse(response, HTTP_Status.OK, None)
+
+
+class IndexAlias(Resource):
+    @request_wrapper
+    def get(self, cluster_name, index_name, alias_name=None):
+        """
+        Get all aliases for this index
+        :param cluster_name: 
+        :param index_name: 
+        :return:
+        """
+        response = IndicesService().get_alias(cluster_name, index_name)
+        return APIResponse(response, HTTP_Status.OK, None)
+
+    @request_wrapper
+    def delete(self, cluster_name, index_name, alias_name):
+        response = IndicesService().remove_alias(cluster_name, index_name, alias_name)
+        return APIResponse(response, HTTP_Status.OK, None)
+
+    @request_wrapper
+    def post(self, cluster_name, index_name, alias_name):
+        response = IndicesService().create_alias(cluster_name, index_name, alias_name)
+        return APIResponse(response, HTTP_Status.OK, None)
+
+
+class IndexMapping(Resource):
+    @request_wrapper
+    def get(self, cluster_name, index_name, mapping_name):
+        response = IndicesService().get_mapping(cluster_name, index_name, mapping_name)
+        return APIResponse(response, HTTP_Status.OK, None)
+
+
+api.add_resource(IndexAction, '/indices/<string:cluster_name>/<string:index_name>/action/<string:action>', endpoint='index_command', methods=['GET'])
+api.add_resource(Index, '/indices/<string:cluster_name>/<string:index_name>', endpoint='index', methods=['GET', 'DELETE', 'POST'])
 api.add_resource(IndexStats, '/indices/<string:cluster_name>/<string:index_names>/_stats', '/indices/<string:cluster_name>/_stats', endpoint='indices_stats', methods=['GET'])
 api.add_resource(IndexList, '/indices/<string:cluster_name>/<string:index_name>', '/indices/<string:cluster_name>', endpoint='indices', methods=['GET'])
+api.add_resource(IndexAlias, '/indices/<string:cluster_name>/<string:index_name>/_alias/<string:alias_name>', endpoint='index_alias', methods=['GET', 'DELETE', 'POST'])
+api.add_resource(IndexMapping, '/indices/<string:cluster_name>/<string:index_name>/_mapping/<string:mapping_name>', endpoint='index_mapping', methods=['GET'])
--- elastichq/service/IndicesService.py
+++ elastichq/service/IndicesService.py
@@ -6,10 +6,57 @@ from ..globals import CONNECTIONS, REQUEST_TIMEOUT
 class IndicesService:
     def get_indices_stats(self, cluster_name, indices_names=None):
         connection = CONNECTIONS.get_connection(cluster_name)
-
         return connection.indices.stats(index=indices_names, request_timeout=REQUEST_TIMEOUT)
 
     def get_indices(self, cluster_name, index_name=None):
         connection = CONNECTIONS.get_connection(cluster_name)
-
         return connection.indices.get(index=index_name or "_all", request_timeout=REQUEST_TIMEOUT)
+
+    def delete_indices(self, cluster_name, index_name):
+        connection = CONNECTIONS.get_connection(cluster_name)
+        return connection.indices.delete(index=index_name, request_timeout=REQUEST_TIMEOUT)
+
+    def create_index(self, cluster_name, index_name, settings=None):
+        connection = CONNECTIONS.get_connection(cluster_name)
+        return connection.indices.create(index=index_name, request_timeout=REQUEST_TIMEOUT)
+
+    def open_index(self, cluster_name, index_name):
+        connection = CONNECTIONS.get_connection(cluster_name)
+        return connection.indices.open(index=index_name, request_timeout=REQUEST_TIMEOUT)
+
+    def close_index(self, cluster_name, index_name):
+        connection = CONNECTIONS.get_connection(cluster_name)
+        return connection.indices.close(index=index_name, request_timeout=REQUEST_TIMEOUT)
+
+    def flush_index(self, cluster_name, index_name):
+        connection = CONNECTIONS.get_connection(cluster_name)
+        return connection.indices.flush(index=index_name, request_timeout=REQUEST_TIMEOUT)
+
+    def refresh_index(self, cluster_name, index_name):
+        connection = CONNECTIONS.get_connection(cluster_name)
+        return connection.indices.refresh(index=index_name, request_timeout=REQUEST_TIMEOUT)
+
+    def clear_cache(self, cluster_name, index_name):
+        connection = CONNECTIONS.get_connection(cluster_name)
+        return connection.indices.clear_cache(index=index_name, request_timeout=REQUEST_TIMEOUT)
+
+    def get_alias(self, cluster_name, index_name):
+        # TODO
+        pass
+
+    def remove_alias(self, cluster_name, index_name, alias_name):
+        # TODO
+        pass
+
+    def create_alias(self, cluster_name, index_name, alias_name):
+        # TODO
+        pass
+
+    def force_merge(self, cluster_name, index_name):
+        connection = CONNECTIONS.get_connection(cluster_name)
+        return connection.indices.forcemerge(index=index_name, request_timeout=REQUEST_TIMEOUT)
+
+    def get_mapping(self, cluster_name, index_name, mapping_name):
+        # TODO: add options here, per: https://www.elastic.co/guide/en/elasticsearch/reference/6.x/indices-get-mapping.html#indices-get-mapping
+        connection = CONNECTIONS.get_connection(cluster_name)
+        return connection.indices.get_mapping(index=index_name, doc_type=mapping_name, request_timeout=REQUEST_TIMEOUT)
