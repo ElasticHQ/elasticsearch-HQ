083a46ab73a07ee76cd9c7a0d416731f7604229a
Will McGinnis <will@predikto.com>
2018-01-12 12:18:04 -0500

    added support for an on-disk user configuration file

--- elastichq/factory.py
+++ elastichq/factory.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from elastichq.api import api_blueprint, public_blueprint
 from elastichq.globals import init_log, init_database, init_marshmallow, init_scheduler
+from elastichq.config.settings import ProdSettings, TestSettings
 
 # noinspection PyUnresolvedReferences
 from elastichq.api import endpoints
@@ -9,13 +10,15 @@ from elastichq.api import endpoints
 __author__ = 'royrusso'
 
 
-def create_app(test=False):
+def create_app(env='PROD'):
     app = Flask(__name__)
 
-    if test is True:
-        app.config.from_object('elastichq.config.test_settings')
+    if env.lower() == 'prod':
+        app.config.from_object(ProdSettings())
+    elif env.lower() == 'test':
+        app.config.from_object(TestSettings())
     else:
-        app.config.from_object('elastichq.config.settings')
+        raise ValueError('Unknown environment: %s' % (env, ))
 
     init_log()
 
@@ -28,8 +31,7 @@ def create_app(test=False):
     # Stop the app from initializing twice in debug mode.
     if not app.debug or os.environ.get("WERKZEUG_RUN_MAIN") == "true":
         # The app is not in debug mode or we are in the reloaded process
-
-        init_database(app, tests=test)
+        init_database(app, tests=env.lower() == 'test')
 
         init_marshmallow(app)
 
