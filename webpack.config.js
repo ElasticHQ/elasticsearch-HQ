24bccec54f7006c65eb4d457601bc76363f5710f
Peter Casanova <peter@predikto.com>
2018-01-31 14:28:06 -0500

    update node modules, introduce webpack prod config and sample output

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ module.exports = {
                 },{
                     loader: 'babel-loader',
                     options: {
-                        presets: ['babel-preset-env']
+                        presets: ['@babel/preset-env']
                     }
                 }]
             },
