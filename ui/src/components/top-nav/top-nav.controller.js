b144eacc06d21af250cba5c9fe2848803ed75a12
Peter Casanova <peter@predikto.com>
2018-02-13 07:52:47 -0500

    updating top nav

--- ui/src/components/top-nav/top-nav.controller.js
+++ ui/src/components/top-nav/top-nav.controller.js
@@ -1,11 +1,23 @@
 import './top-nav.style.scss'
 
 class topNavController {
-    constructor($state, $transitions, $location) {
+    constructor(ClusterConnection, Notification, $state, $transitions, $location, $rootScope) {
         'ngInject';
 
         // Get URL to determin if ROOT or not
         this.$location = $location;
+
+        this.service = ClusterConnection;
+        this.$state = $state;
+        this.Notification = Notification;
+
+
+        // when adding / removing a cluster from settings
+        $rootScope.$on('clusters.refresh', (event, data) => {
+            this.getClusters();
+        });
+
+        this.getClusters();
 
         this.seeIfNotRoot();
 
@@ -17,6 +29,17 @@ class topNavController {
 
     seeIfNotRoot(){
         this.isNotRoot = this.$location.$$path !== '/';
+    }
+
+    getClusters(reloading) {
+        this.service.getClusters().then((resp) => {
+            this.clusters = resp.data.data;
+            if (!!reloading) this.Notification.success({message: `Found ${this.clusters.length} clusters`, delay: 3000});
+            console.log("---- clusters: ", this.clusters)
+        }, (err) => {
+            this.Notification.error({message: 'Error getting clusters'});
+            console.log('---- get clusters error: ', err)
+        })
     }
 
     
