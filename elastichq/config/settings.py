4a1bf2cae5b4dd9277b1a516335083b39425d722
Roy Russo <royrusso@gmail.com>
2018-02-21 16:45:41 -0500

    - rev 3.0.3

--- elastichq/config/settings.py
+++ elastichq/config/settings.py
@@ -116,7 +116,7 @@ class ProdSettings(BaseSettings):
     # static
     HQ_SITE_URL = 'http://elastichq.org'
     HQ_GH_URL = 'https://github.com/ElasticHQ/elasticsearch-HQ'
-    API_VERSION = '3.0.2'
+    API_VERSION = '3.0.3'
     SCHEDULER_JOBSTORES = {
         'default': MemoryJobStore()
     }
