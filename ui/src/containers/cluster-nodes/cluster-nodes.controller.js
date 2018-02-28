d190cd5c25e4b6ffd5ced1d99f956ba07220294b
Peter Casanova <peter@predikto.com>
2018-02-28 17:03:08 -0500

    tmp staging

--- ui/src/containers/cluster-nodes/cluster-nodes.controller.js
+++ ui/src/containers/cluster-nodes/cluster-nodes.controller.js
@@ -10,9 +10,23 @@ class clusterNodesController {
 
     this.connected = false;
 
+
     this.clusterName = $stateParams.clusterName;
+
+    // Try to get URL 
+    //  even if path is being forwared by a Apache / Nginx conf.  
+    //  i.e. http://my_site.com/my/custom/path is the root of the python app
+    console.log('---- window.location', window.location)
+    let baseUrl = window.location.href.split('/#!')[0];
+
+    if (/\:8080/.test(baseUrl)) baseUrl = 'http://localhost:5005';
     
-    this.socket = io(`ws/clusters/${this.clusterName}/nodes`);
+    // Websockets do not work with relative paths, so get absolute and append the WS portion
+    //  upgrade from HTTP to WS should be automatic
+    baseUrl = baseUrl += `/socket/clusters/${this.clusterName}/nodes`;
+    console.log('---- baseUrl: ', baseUrl)
+    
+    this.socket = io(baseUrl);
     this.socket.on('connect', () => { this.connected = true })
     this.socket.on('disconnect', () => { this.connected = false })
     this.socket.on('event', (data) => { this.message(data); })
