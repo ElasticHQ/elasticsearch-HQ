5ca1b63170d7c00d3613adba564ad1ed5482dc00
Peter Casanova <peter@predikto.com>
2018-02-28 14:27:06 -0500

    336: Stub

---
+++ ui/src/containers/cluster-nodes/cluster-nodes.controller.js
@@ -0,0 +1,12 @@
+import './cluster-nodes.style.scss'
+
+class clusterNodesController {
+
+  // Imports go here
+  constructor() {
+    'ngInject';
+    
+  }
+}
+
+export default clusterNodesController;
