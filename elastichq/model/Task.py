7f627b916d6a5c9f9befa302dd39478282605cfa
Roy Russo <royrusso@gmail.com>
2018-03-02 16:54:32 -0500

    #337 - added node stats call

--- elastichq/model/Task.py
+++ elastichq/model/Task.py
@@ -2,6 +2,7 @@
 import json
 
 import eventlet
+import jmespath
 
 from elastichq.service import NodeService
 from ..globals import LOG, socketio
@@ -54,21 +55,32 @@ class Task:
             LOG.debug('-----------------------------------------')
 
             if self.metric == 'nodes':
-                nodes_stats = NodeService().get_node_stats(self.cluster_name, request_timeout=self.task_timeout)
+                node_stats = NodeService().get_node_stats(self.cluster_name, request_timeout=self.task_timeout)
+                node_ids = list(node_stats['nodes'].keys())
                 nodes = []
-                for node in nodes_stats:
-                    node = {
-                        "node_id": node.get('id', None),
-                        "name": node.get('n', None),
-                        "heapCurrent": node.get('hc', None),
-                        "heapMax": node.get('hm', None),
-                        "ramCurrent": node.get('rc', None),
-                        "ramMax": node.get('rm', None),
-                        "fielddataMemory": node.get('fm', None),
-                        "load": node.get('l', 0),
-                        "uptime": node.get('u', 0)
-                    }
+                for node_id in node_ids:
+                    node_dict = node_stats['nodes'][node_id]
+
+                    available_in_bytes = jmespath.search("fs.data[0].available_in_bytes", node_dict)
+                    total_in_bytes = jmespath.search("fs.data[0].total_in_bytes", node_dict)
+
+                    node = \
+                        {
+                            "node_id": node_id,
+                            "name": jmespath.search("name", node_dict),
+                            "host": jmespath.search("host", node_dict),
+                            "heap_max_in_bytes": jmespath.search("jvm.mem.heap_max_in_bytes", node_dict),
+                            "heap_used_in_bytes": jmespath.search("jvm.mem.heap_used_in_bytes", node_dict),
+                            "docs_count": jmespath.search("indices.docs.deleted", node_dict),
+                            "docs_deleted": jmespath.search("indices.docs.count", node_dict),
+                            "store_size": jmespath.search("indices.store.size_in_bytes", node_dict),
+                            "cpu_percent": jmespath.search("process.cpu.percent", node_dict),
+                            "field_data_cache_in_bytes": jmespath.search("indices.fielddata.memory_size_in_bytes", node_dict),
+                            "fs_used_in_bytes" : total_in_bytes - available_in_bytes,
+                            "index_total" : jmespath.search("indices.indexing.index_total", node_dict)
+                        }
                     nodes.append(node)
+                LOG.debug("Broadcast to room: " + self.room_name)
                 socketio.emit('event', {'data': json.dumps(nodes)}, room=self.room_name, namespace="/ws")
             else:
                 pass
