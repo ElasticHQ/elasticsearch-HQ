52b85277ee3158966fb0f9c6ed654435cb04f68d
Roy Russo <royrusso@gmail.com>
2018-03-16 12:17:09 -0400

    Fixes #359

--- manage.py
+++ manage.py
@@ -6,13 +6,67 @@ __author__ = 'royrusso'
 import os
 
 from flask_migrate import MigrateCommand
-from flask_script import Command, Manager
+from flask_script import Command, Manager, Option, Server as _Server
 
-from elastichq.factory import create_app
-from elastichq.globals import db
+from elastichq import create_app
+from elastichq.globals import db, socketio
 
-app = create_app()
-manager = Manager(app)
+manager = Manager(create_app)
+
+class Server(_Server):
+    """
+    From https://github.com/miguelgrinberg/flack/blob/master/manage.py
+    This is the only way to call ./manage.py runserver, as flask-socketio blocks the call otherwise.
+    """
+
+    help = description = 'Runs the Socket.IO web server'
+
+    def get_options(self):
+        options = (
+            Option('-H', '--host',
+                   dest='host',
+                   default=self.host),
+
+            Option('-P', '--port',
+                   dest='port',
+                   type=int,
+                   default=self.port),
+
+            Option('-d', '--debug',
+                   action='store_true',
+                   dest='use_debugger',
+                   help=('enable the Werkzeug debugger (DO NOT use in '
+                         'production code)'),
+                   default=self.use_debugger),
+
+            Option('-r', '--reload',
+                   action='store_true',
+                   dest='use_reloader',
+                   help=('monitor Python files for changes (not 100%% safe '
+                         'for production use)'),
+                   default=self.use_reloader),
+            Option('-R', '--no-reload',
+                   action='store_false',
+                   dest='use_reloader',
+                   help='do not monitor Python files for changes',
+                   default=self.use_reloader),
+        )
+        return options
+
+    def __call__(self, app, host, port, use_debugger, use_reloader):
+        # override the default runserver command to start a Socket.IO server
+        if use_debugger is None:
+            use_debugger = app.debug
+            if use_debugger is None:
+                use_debugger = True
+        if use_reloader is None:
+            use_reloader = app.debug
+        socketio.run(app,
+                     host=host,
+                     port=port,
+                     debug=use_debugger,
+                     use_reloader=use_reloader,
+                     **self.server_options)
 
 
 class CleanDB(Command):
@@ -21,6 +75,7 @@ class CleanDB(Command):
     """
 
     def run(self):
+        app = create_app()
         db.drop_all(app=app)
 
 
@@ -36,6 +91,7 @@ class PyTest(Command):
         return exit_code
 
 
+manager.add_command("runserver", Server())
 manager.add_command('clean-db', CleanDB)
 manager.add_command('run-tests', PyTest)
 
