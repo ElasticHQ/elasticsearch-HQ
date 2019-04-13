f80f8c5f29e16500afbcbde905e67049ec2573fc
Roy Russo <royrusso@gmail.com>
2019-04-13 18:47:17 -0400

    Fixes #416

--- tests/scripts/start_clusters.sh
+++ tests/scripts/start_clusters.sh
@@ -1,8 +1,11 @@
+#!/usr/bin/env bash
 /Users/royrusso/dev/elasticsearch/elasticsearch-2.4.6/bin/elasticsearch &
 P1=$!
 /Users/royrusso/dev/elasticsearch/elasticsearch-5.6.5/bin/elasticsearch &
 P2=$!
-/Users/royrusso/dev/elasticsearch/elasticsearch-6.1.1/bin/elasticsearch
+/Users/royrusso/dev/elasticsearch/elasticsearch-6.1.1/bin/elasticsearch &
 P3=$!
-wait $P1 $P2 $P3
+/Users/royrusso/dev/elasticsearch/elasticsearch-7.0.0/bin/elasticsearch
+P4=$!
+wait $P1 $P2 $P3 $P4
 
